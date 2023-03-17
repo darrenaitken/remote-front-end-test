37122866b525f4b2e6076937707e84f2d2d61489
Darren Aitken <darren.aitken@live.co.uk>
2023-03-17 11:46:06 +0000

    Changed parameters

--- src/components/SortAndFilter/tests/SortAndFilter.spec.jsx
+++ src/components/SortAndFilter/tests/SortAndFilter.spec.jsx
@@ -4,28 +4,10 @@ import userEvent from '@testing-library/user-event';
 
 import SortAndFilter from '../SortAndFilter';
 
-const mockListing = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: 'CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage:
-        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
-};
-
-const mockListings = Array(5)
-    .fill(mockListing)
-    .map((listing, index) => ({ ...listing, id: index }));
-
 describe('SortAndFilter', () => {
     it('should render all options on the screen', async () => {
-        render(<SortAndFilter listing={mockListings} />);
+        const onChange = () => {};
+        render(<SortAndFilter onChange={onChange} />);
 
         const propertyType = screen.getByLabelText('Property Type');
         expect(propertyType).toBeInTheDocument();
